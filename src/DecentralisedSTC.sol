5f8206e82bf3645c05a71b1f703c05506a02a414
Divyanshh Kalra <divyanshhkalra1234@gmail.com>
2025-01-02 22:30:31 +0530

    done

---
+++ src/DecentralisedSTC.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+/*
+* @title: DecentralisedSTC
+* @author: DivK
+* @ Collateral : EXOGENOUS (ETH & BTC)
+* Minting : Algorithmic
+* Relative Stability : Pegged to  USD
+* Governed  by DSCEngine : Just Implementation of ERC20 Contract
+*/
+ 
+ contract DecentralisedSTC{
+    
+ }
