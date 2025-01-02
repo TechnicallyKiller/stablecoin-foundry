9a80f20e5d83f8b96d1b5ecf50284e9994e78f3c
Divyanshh Kalra <divyanshhkalra1234@gmail.com>
2025-01-02 23:29:05 +0530

    Onto DSC Engine

---
+++ script/DeploymentDSTC.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {DecentralisedSTC} from "../src/DecentralisedSTC.sol";
+
+contract DeploymentDSTC is Script{
+    DeploymentDSTC deployer;
+    function run() public  returns (DeploymentDSTC){
+        vm.startBroadcast();
+        deployer= new DeploymentDSTC();
+        vm.stopBroadcast();
+        return deployer;
+
+    }
+}
--- src/DecentralisedSTC.sol
+++ src/DecentralisedSTC.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
 
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 // Layout of Contract:
 // version
 // imports
@@ -33,8 +35,30 @@ pragma solidity ^0.8.10;
 * Governed  by DSCEngine : Just Implementation of ERC20 Contract
 */
  
- contract DecentralisedSTC{
-     constructor(){
-        
+ contract DecentralisedSTC is ERC20Burnable,Ownable{
+    error DSTC_MUST_BE_MORE_THAN_ZERO();
+    error DSTC_ExceedsBurnAmount();
+    error DSTC_NotZeroAddress();
+     constructor() ERC20 ("DecentralisedSTC1","DSTC"){}
+
+     function burn(uint256 _amount) public override onlyOwner{
+        uint256 balance = balanceOf(msg.sender);
+        if(_amount <=0){
+            revert DSTC_MUST_BE_MORE_THAN_ZERO();
+        }
+        if(_amount>=balance){
+            revert DSTC_ExceedsBurnAmount();
+        }
+        super.burn(_amount);
+     }
+     function mint(address _to , uint256 amount) external onlyOwner returns(bool){
+        if(_to==address(0)){
+            revert DSTC_NotZeroAddress();
+        }
+        if(amount<=0){
+            revert DSTC_MUST_BE_MORE_THAN_ZERO();
+        }
+        _mint(_to,amount);
+        return true;
      }
  }
