8a73533dec23af04ba4abffac0f395faa3c0b562
Divyanshh Kalra <divyanshhkalra1234@gmail.com>
2025-01-03 18:47:35 +0530

    DSCEngine

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.10;
 
 
 // This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+import {DecentralisedSTC} from "./DecentralisedSTC.sol";
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // Layout of Contract:
 // version
 // imports
@@ -25,14 +28,34 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 // internal
 // private
 // view & pure functions
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
 ///////////////////
 //   Errors  //
 ///////////////////
 error DSCEngine_lessthanZero();
+error DSCEngine_NOTallowedToken();
+error DSCEngine_pricefeedaddresss_Notequal_tokenaddress_length();
+error DSCEngine_transferFailed();
+
+///////////////////
+//  State Variables  //
+///////////////////
+
+mapping(address token => address pricefeed) private s_priceFeed;
+mapping(address user => mapping(address token => uint256 amount)) private s_collatDeposited;
+DecentralisedSTC private i_dsc; //token to pricefeed
+
+///////////////////
+//   EVENTS  //
+///////////////////
+
+event CollateralDeposited(address indexed depositor , address indexed token , uint256 deposited);
+
+
 ///////////////////
 //   Modifiers  //
 ///////////////////
+
 modifier morethanZero(uint256 amount) {
     if(amount==0){
         revert DSCEngine_lessthanZero();
@@ -40,12 +63,28 @@ modifier morethanZero(uint256 amount) {
     _;
 }
 
+modifier isAllowedToken(address token) {
+   if(s_priceFeed[token]==address(0)){
+    revert DSCEngine_NOTallowedToken();
+   } 
+   _;
+}
+
 
 ///////////////////
 //   Functions   //
 ///////////////////
 
-constructor(){}
+constructor(address[] memory tokenAdresses, address[] memory pricefeedAddressess, address DscAddress){
+    if(tokenAdresses.length!=pricefeedAddressess.length){
+        revert DSCEngine_pricefeedaddresss_Notequal_tokenaddress_length();
+    }
+    for(uint256 i=0;i< tokenAdresses.length;i++){
+        s_priceFeed[tokenAdresses[i]]=pricefeedAddressess[i];
+    }
+    i_dsc= DecentralisedSTC(DscAddress);
+
+}
 
 ///////////////////////////
 //   External Functions  //
@@ -59,7 +98,16 @@ constructor(){}
     function depositCollateral(
         address tokenCollateralAddress,
         uint256 amountCollateral
-    ) external morethanZero(amountCollateral) {
+    ) external morethanZero(amountCollateral)
+            isAllowedToken(tokenCollateralAddress)
+            nonReentrant
+     {
+        s_collatDeposited[msg.sender] [tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+       bool success =  IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this),amountCollateral);
+       if(!success){
+            revert DSCEngine_transferFailed();
+       }
 
     }
 
